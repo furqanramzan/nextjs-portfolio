2f745f5bbd4b7e4a4e93710171dbbb564167cd28
Muhammad Furqan <furqanramzan271996@gmail.com>
2023-08-22 19:38:24 +0500

    fix(edge): undefined node env variable issue

--- utils/constants.ts
+++ utils/constants.ts
@@ -4,7 +4,7 @@ const constants = z
   .object({
     NEXT_PUBLIC_APP_NAME: z.string(),
     JWT_SECRET: z.string(),
-    NODE_ENV: z.string(),
+    NODE_ENV: z.string().optional(),
   })
   // eslint-disable-next-line n/prefer-global/process
   .parse(process.env);
